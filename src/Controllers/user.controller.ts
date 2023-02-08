2d87602e2655bde7e57ad7e127181eaf3f5156a7
propah <deltiumpvp@gmail.com>
2023-02-08 11:09:39 +0100

    Add verification for user in db

--- src/Controllers/user.controller.ts
+++ src/Controllers/user.controller.ts
@@ -2,19 +2,27 @@ import { Request, Response } from 'express';
 import logging from "../Config/logging";
 import { User, IUser } from "../Models/user.model";
 import asyncHandler from 'express-async-handler';
-import { Jwt } from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
 const NAMESPACE = "UserController";
 
-function validateUserInfo(user: IUser) {
+function validateUserInfoFormat(user: IUser): Error | undefined {
     if (!(user.email && /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(user.email))) {
-        throw new Error("Invalid email");
+        return Error("Invalid email");
     }
     if (user.email.length > 50) throw new Error("Email is too long");
-    if (!(/^[A-Za-z][A-Za-z0-9_]{3,29}$/).test(user.username)) throw new Error("Invalid username characters");
-    if (user.username.length < 5 || user.username.length > 20) throw new Error("Invalid username length");
-    if (user.password.length < 8 || user.password.length > 50) throw new Error("Invalid password length");
+    if (!(/^[A-Za-z][A-Za-z0-9_]{3,29}$/).test(user.username)) return Error("Invalid username characters");
+    if (user.username.length < 4 || user.username.length > 20) return Error("Invalid username length");
+    if (user.password.length < 8 || user.password.length > 50) return Error("Invalid password length");
+}
+
+function generateJWT(id: string): String {
+    if (!process.env.JWT_SECRET) {
+        logging.error(NAMESPACE, "JWT_SECRET is undefined");
+        throw new Error("JWT_SECRET is undefined");
+    } 
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
 }
 
 // @desc   Register a user
@@ -24,25 +32,24 @@ const registerUser = asyncHandler(async (req: Request, res: Response) => {
     try {
         logging.debug(NAMESPACE, `Registering user ${req.body.username}`, req.socket.remoteAddress);
         const user = new User(req.body);
-        validateUserInfo(user);
+        const error: Error | undefined = validateUserInfoFormat(user);
+        if (error) res.status(400).json({ message: error.message });
         if (await User.findOne({ email: user.email })) {
             res.status(400).json({ message: `Email ${user.email} is already taken` });
-            return;
-        }
-        if (await User.findOne({ username: user.username })) {
+        } else if (await User.findOne({ username: user.username })) {
             res.status(400).json({ message: `Username ${user.username} is already taken` });
-            return;
+        } else {
+            logging.debug(NAMESPACE, `Registered user ${req.body.username}`, req.socket.remoteAddress);
+            user.password = await bcrypt.hash(user.password, 10);
+            await user.save();
+            res.status(200).json({ username: user.username, email: user.email, token: generateJWT(user._id.toString()) });
         }
-        await user.save();
-        user.password = await bcrypt.hash(user.password, 10);
-        logging.debug(NAMESPACE, `Registered user ${req.body.username}`, req.socket.remoteAddress);
-        res.status(200).json({username: user.username, email: user.email});
     } catch (err: any) {
-        res.status(400);
         if (err instanceof Error) {
-            logging.error(NAMESPACE, `Could not register user, invalid request: ${err}`, req.socket.remoteAddress);
+            logging.error(NAMESPACE, `Could not register user: ${err}`, req.socket.remoteAddress);
         }
-        res.status(400).json({ message: `Could not register user, invalid request` });
+        res.status(400);
+        throw new Error("Could not register");
     }
 });
 
@@ -51,21 +58,30 @@ const registerUser = asyncHandler(async (req: Request, res: Response) => {
 // @access Public
 const loginUser = asyncHandler(async (req: Request, res: Response) => {
     try {
-
-    } catch (err: any) {
-        res.status(400);
-        if (err instanceof Error) {
-            logging.error(NAMESPACE, `Could not login user, invalid request: ${err}`, req.socket.remoteAddress);
+        if (!(req.body.email && req.body.password)) throw new Error("missing email or password");
+        logging.debug(NAMESPACE, `Logging in user ${req.body.email}`, req.socket.remoteAddress);
+        const user = await User.findOne({ email: req.body.email });
+        if (user && (await bcrypt.compare(req.body.password, user.password))) {
+            logging.debug(NAMESPACE, `Logged in user ${req.body.email}`, req.socket.remoteAddress);
+            res.status(200).json({username: user.username, email: user.email, token: generateJWT(user._id.toString()) });
+        } else {
+            logging.debug(NAMESPACE, `Invalid credentials for user ${req.body.email}`, req.socket.remoteAddress);
+            res.status(400).json({ message: `Invalid credentials` });
         }
-        res.status(400).json({ message: `Could not login user, invalid request` });
+    } catch (err: any) {
+        if (err instanceof Error) {
+            logging.error(NAMESPACE, `Could not login user: ${err}`, req.socket.remoteAddress);
+        }
+        res.status(400);
+        throw new Error("Could not login");
     }
 });
 
 // @desc   Register a user
 // @route  POST /api/users/me
-// @access Public
+// @access Private
 const getOwnUserData = asyncHandler(async (req: Request, res: Response) => {
-    res.send(req);
+    res.status(200).json(res.locals.user);
 });
 
 export {
