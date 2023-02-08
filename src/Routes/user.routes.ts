896623deed88af96ea04ce75e7b29233ad5e6867
propah <deltiumpvp@gmail.com>
2023-02-08 11:11:17 +0100

    Use auth middleware to protect creative routes

--- src/Routes/user.routes.ts
+++ src/Routes/user.routes.ts
@@ -1,10 +1,12 @@
 import express from "express";
 const userRouter = express.Router();
 import { registerUser, loginUser, getOwnUserData } from "../Controllers/user.controller";
+import protect from "../Middleware/auth.middleware";
+
 const NAMESPACE = "UserRoutes";
 
 userRouter.post("/users/register", registerUser);
 userRouter.post("/users/login", loginUser);
-userRouter.get("/users/me", getOwnUserData);
+userRouter.get("/users/me", protect, getOwnUserData);
 
 export = userRouter
