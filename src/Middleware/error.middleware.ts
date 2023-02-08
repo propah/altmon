4b7c828f9d94ce930704f74240ff0b17f8cf7239
propah <deltiumpvp@gmail.com>
2023-02-08 11:10:16 +0100

    Fix error for error middleware

--- src/Middleware/error.middleware.ts
+++ src/Middleware/error.middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import logging from '../Config/logging';
 const NAMESPACE = 'ErrorMiddleware';
 const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    const status_code = res.statusCode ? 500 : res.statusCode;
+    const status_code = res.statusCode ? res.statusCode : 500;
     res.status(status_code).json({
         message: err.message,
         stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack
