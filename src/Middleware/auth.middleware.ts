61f457017d03444f6eb49ed8fc55e437184186f5
propah <deltiumpvp@gmail.com>
2023-02-09 14:35:56 +0100

    Add admin role checking middleware

--- src/Middleware/auth.middleware.ts
+++ src/Middleware/auth.middleware.ts
@@ -6,21 +6,26 @@ import logging from "../Config/logging";
 
 const NAMESPACE = "AuthMiddleware";
 
-const protect = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+
+function getDecodedToken(req: Request, res: Response): JwtPayload {
     if (!process.env.JWT_SECRET) {
         logging.error(NAMESPACE, "JWT_SECRET is undefined");
         throw new Error("JWT_SECRET is undefined");
     }
     let token;
+    if (!req.headers["x-access-token"]) throw new Error("Not authorized");
+    token = req.headers["x-access-token"].toString();
+    const decoded: JwtPayload = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+    return decoded;
+}
+
+const protect = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
     try {
-        if (!req.headers["x-access-token"]) {
-            res.status(401).json({ message: "Not authorized" });
-            return;
-        }
-        token = req.headers["x-access-token"].toString();
-        const decoded = jwt.verify(token, process.env.JWT_SECRET) as JwtPayload;
+        const decoded = getDecodedToken(req, res);
         const user = await User.findById(decoded.id);
+        if (!user) throw new Error("User not found");
         res.locals.user = user;
+        res.locals.admin = false;
         next();
     } catch (err) {
         if (err instanceof Error) {
@@ -31,5 +36,34 @@ const protect = asyncHandler(async (req: Request, res: Response, next: NextFunct
     }
 });
 
+// Auth middleware that restrict the use of the route to admin only
+const adminOnly = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        let user: IUser | null | undefined = res.locals.user;
+        if (!user) {
+            // If you see this it means the protect middleware is not used and should be
+            logging.error(NAMESPACE, "User not found in res.locals, trying to find it in the database");
+            const decoded = getDecodedToken(req, res);
+            user = await User.findById(decoded.id);
+            if (!user) throw new Error("User not found");
+        }
+        if (user.role !== "admin") {
+            res.locals.admin = false;
+            res.status(401).json({message: "Not authorized"});
+            return;
+        }
+        res.locals.admin = true;
+        next();
+    } catch (err) {
+        if (err instanceof Error) {
+            logging.error(NAMESPACE, `Could not authenticate user: ${err}`, req.socket.remoteAddress);
+        }
+        res.status(401);
+        throw new Error('Not authorized');
+    }
+});
 
-export = protect;
+export {
+    protect,
+    adminOnly
+}
